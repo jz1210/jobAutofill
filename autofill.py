8377a62a765aaf506fcc424067802ffcf42bb3f6
nameless-Q <john.zhu@gordon.edu>
2025-02-17 19:50:46 -0500

    autofill demo 1
    added some feature for first autofill demo edition

--- autofill.py
+++ autofill.py
@@ -9,11 +9,8 @@ class JobApplicationAutofiller:
         # Load user data
         with open(config_path) as f:
             self.user_data = json.load(f)
-        
-        # State abbreviation standardization
-        # self.user_data['address']['state'] = self.user_data['address']['state'].upper()
 
-    def _fill_current_page(self):
+    def _fill_name(self):
         try:
             # Personal Information
             # Get all elements whose id attribute contains first name
@@ -32,24 +29,50 @@ class JobApplicationAutofiller:
             print(f"Error: {str(e)}")
 
     def _handle_address(self):
-        # Autofill the address
-        self.page.ele('@id:addressLine1').input(self.user_data['address']['street'])
-        self.page.ele('@id:city').input(self.user_data['address']['city'])
-        self.page.ele('@|id:Region@|id:state@|id:State@|id:region').click()
-        self.page.ele(f'text:{self.user_data['address']['state']}', timeout=5).click()
-        self.page.ele('@|id:zipCode@|id:postalCode').input(self.user_data['address']['zip_code'])
+        try:
+            # Autofill the address
+            self.page.ele('@id:addressLine1').input(self.user_data['address']['street'])
+            self.page.ele('@id:city').input(self.user_data['address']['city'])
+            self.page.ele('@|id:Region@|id:state@|id:State@|id:region').click()
+            self.page.ele(f'text:{self.user_data['address']['state']}', timeout=5).click()
+            self.page.ele('@|id:zipCode@|id:postalCode').input(self.user_data['address']['zip_code'])
+        
+        except Exception as e:
+            print(f"Error: {str(e)}")
 
-    def _fill_phone_number(self):
-        # Autofill the phone number
-        pnumber = self.page.eles('@|id:phone@|id:phoneNumber')
-        for phone in pnumber:
-            phone.input(self.user_data['phone'])
+    def _fill_phone_info(self):
+        try:
+            self.page.ele('@id:phoneType').click()
+            self.page.ele('text:Mobile', timeout=5).click()
+            # Autofill the phone number
+            pnumber = self.page.eles('@|id:phone@|id:phoneNumber')
+            for phone in pnumber:
+                phone.input(self.user_data['phone'])
+        
+        except Exception as e:
+            print(f"Error: {str(e)}")
+
 
     def apply_to_current_page(self):
         """Execute form filling on current browser tab"""
-        self._fill_current_page()
-        self._handle_address()
-        self._fill_phone_number()
+        #self._fill_name()
+        #self._handle_address()
+        #self._fill_phone_info()
+        self._self_identity_part()
+
+    def _self_identity_part(self):
+        try:
+        # Autofill the address
+            self.page.ele('@id:gender').click()
+            self.page.ele(f'text:{self.user_data['gender']}', timeout=5).click()
+            self.page.ele('@|id:race@|id:ethnicity').click()
+            self.page.ele(f'{self.user_data['race']}', timeout=5).click()
+            self.page.ele('@|id:veteran@|id:veteranStatus').click()
+            self.page.ele(f'{self.user_data['veteran_status']}', timeout=5).click()
+
+        except Exception as e:
+            print(f"Error: {str(e)}")
+
 
 if __name__ == "__main__":
     # Connect to existing browser and apply
